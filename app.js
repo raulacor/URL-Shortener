ac834ad7b5667430d4f47084f384a0622c47c0cf
raulacor <raul.acorsi@gmail.com>
2025-11-02 13:43:29 -0300

    Remove: Unecessary variables

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ document.addEventListener("DOMContentLoaded", () => {
     if (storedURLss){
         storedURLss.forEach((URLs) => urlss.push(URLs));
         updateURLssList();
-        updateStats();
     };
 });
 
@@ -24,7 +23,6 @@ const addURLs = () => {
         urlsInput.value= "";
         
         updateURLssList();
-        updateStats();
         saveURLs();
     }
 };
@@ -35,24 +33,9 @@ const copyURLs = (index) => {
 const deleteURLs = (index) => {
     urlss.splice(index, 1);
     updateURLssList();
-    updateStats();
     saveURLs();
 };
 
-const updateStats = () => {
-    const completedURLss = urlss.filter(urls => urls.completed).length;
-    const totalURLss = urlss.length;
-    const progress = (completedURLss / totalURLss)*100;
-    const progressBar = document.getElementById('progress')
-
-    progressBar.style.width= `${progress}% `
-
-    document.getElementById("numbers").innerText = `${completedURLss} / ${totalURLss}`;
-
-    if(urlss.length && completedURLss === totalURLss) {
-        blastConfetti();
-    };
-};
 
 const updateURLssList = () => {
     const urlsList = document.getElementById('urls-list')
@@ -74,50 +57,7 @@ const updateURLssList = () => {
         listItem.addEventListener("change", () => toggleURLsComplete(index));
         urlsList.append(listItem);
     });
-};
-
-const blastConfetti = () => {
-    const count = 200,
-    defaults = {
-    origin: { y: 0.7 },
-    };
-
-    function fire(particleRatio, opts) {
-    confetti(
-        Object.assign({}, defaults, opts, {
-        particleCount: Math.floor(count * particleRatio),
-        })
-    );
-    };
-
-    fire(0.25, {
-    spread: 26,
-    startVelocity: 55,
-    });
-
-    fire(0.2, {
-    spread: 60,
-    });
-
-    fire(0.35, {
-    spread: 100,
-    decay: 0.91,
-    scalar: 0.8,
-    });
-
-    fire(0.1, {
-    spread: 120,
-    startVelocity: 25,
-    decay: 0.92,
-    scalar: 1.2,
-    });
-
-    fire(0.1, {
-    spread: 120,
-    startVelocity: 45,
-    });
-};
-
+};  
 document.getElementById('newURLs').addEventListener('click', function(e){
     e.preventDefault();
 
