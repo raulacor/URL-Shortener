c2e81c1d925318abd9ec8e882b6a8d80e61db3e9
raulacor <raul.acorsi@gmail.com>
2025-11-03 14:14:41 -0300

    Add: short URL Copy counter; Fix: Floating label

--- static/app.js
+++ static/app.js
@@ -20,19 +20,20 @@ const addURLs = async () => {
     try {
         const res = await fetch("http://127.0.0.1:5000/shorten", {
             method: "POST",
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify({url: originalURL})
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ url: originalURL })
         });
 
         const data = await res.json();
 
         if (data.short_url) {
-            urlss.push({ text: originalURL, short: data.short_url });
+            // Adiciona contador de cópias ao novo item
+            urlss.push({ text: originalURL, short: data.short_url, copies: 0 });
             urlsInput.value = "";
             updateURLssList();
             saveURLs();
         } else {
-            alert("Errot couldn't shorten URL");
+            alert("Erro: não foi possível encurtar a URL");
         }
     } catch (err) {
         console.error("Err:", err);
@@ -41,9 +42,17 @@ const addURLs = async () => {
 
 const copyURLs = (index) => {
     const urlToCopy = urlss[index].short;
+
     navigator.clipboard.writeText(urlToCopy)
-        .then(() => alert(`copied: ${urlToCopy}`))
-        .catch(err => console.error("couldn't copy", err));
+        .then(() => {
+            // Incrementa contador de cópias
+            urlss[index].copies = (urlss[index].copies || 0) + 1;
+            saveURLs();
+            updateURLssList();
+
+            alert(`Copiado: ${urlToCopy}`);
+        })
+        .catch(err => console.error("Erro ao copiar", err));
 };
 
 const deleteURLs = (index) => {
@@ -61,11 +70,12 @@ const updateURLssList = () => {
         listItem.innerHTML = `
         <div class="urlsItem">
             <div class="urls">
-                <p><a>${urls.short}</a></p>
+                <p><a href="${urls.short}" target="_blank">${urls.short}</a></p>
+                <small>Copiado ${urls.copies || 0} ${urls.copies === 1 ? 'vez' : 'vezes'}</small>
             </div>
             <div class="icons">
-                <img src="./static/img/copy.png" onClick="copyURLs(${index})" />
-                <img src="./static/img/bin.png" onClick="deleteURLs(${index})" />
+                <img src="./static/img/copy.png" onClick="copyURLs(${index})" title="Copiar URL" />
+                <img src="./static/img/bin.png" onClick="deleteURLs(${index})" title="Excluir URL" />
             </div>
         </div>
         `;
@@ -73,7 +83,7 @@ const updateURLssList = () => {
     });
 };
 
-document.getElementById('newURLs').addEventListener('click', function(e){
+document.getElementById('newURLs').addEventListener('click', function (e) {
     e.preventDefault();
     addURLs();
 });
